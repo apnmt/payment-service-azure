d69439fe59f975a8804fdf28032775614032cfc6
TWI <audi-connect@msg.group>
2022-01-11 10:24:02 +0100

    initial commit

---
+++ src/main/java/de/apnmt/payment/paymentserviceaws/PaymentServiceAwsApplication.java
@@ -0,0 +1,13 @@
+package de.apnmt.payment.paymentserviceaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/apnmt/payment/paymentserviceaws/PaymentServiceAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.apnmt.payment.paymentserviceaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
