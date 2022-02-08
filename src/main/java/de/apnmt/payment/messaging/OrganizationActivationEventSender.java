495149184cc1076c41e181882bdbbe84795a7bbb
tobi5775 <tobias.wittmann96@web.de>
2022-02-08 15:46:17 +0100

    add messaging via Azure ServiceBus

--- src/main/java/de/apnmt/payment/messaging/OrganizationActivationEventSender.java
+++ src/main/java/de/apnmt/payment/messaging/OrganizationActivationEventSender.java
@@ -5,6 +5,7 @@ import de.apnmt.common.event.value.OrganizationActivationEventDTO;
 import de.apnmt.common.sender.ApnmtEventSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,9 +13,16 @@ public class OrganizationActivationEventSender implements ApnmtEventSender<Organ
 
     private static final Logger log = LoggerFactory.getLogger(OrganizationActivationEventSender.class);
 
+    private JmsTemplate jmsTemplate;
+
+    public OrganizationActivationEventSender(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
     @Override
     public void send(String topic, ApnmtEvent<OrganizationActivationEventDTO> event) {
         log.info("Send event {} to topic {}", event, topic);
+        jmsTemplate.convertAndSend(topic, event);
     }
 
 }
